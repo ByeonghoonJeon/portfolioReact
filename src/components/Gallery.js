f494464a6b5fecf034211fe0dc20d167fa99ae79
Nathan Jeon <jeon4972@snu.ac.kr>
2021-08-12 15:20:30 -0400

    Practice

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -2,6 +2,19 @@ import React, { Component } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Cards from "./PortfolioCards";
+import { PORTFOLIO } from "../shared/portfolio";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button,
+  Breadcrumb,
+  BreadcrumbItem,
+} from "reactstrap";
+import { Link } from "react-router-dom";
 
 class Gallery extends Component {
   render() {
